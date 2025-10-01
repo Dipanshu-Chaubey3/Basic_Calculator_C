7b701c86a8b16f54cd16af1ae68c771200bcc45a
Dipanshu-Chaubey3 <dchaubey891@gmail.com>
2025-10-01 14:54:06 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,7 +3,6 @@
 int main() {
     char operator;
     double num1, num2, result;
-    // Prompt user for input
     printf("Enter first number: ");
     scanf("%lf", &num1);
     printf("Enter an operator (+, -, *, /): ");
